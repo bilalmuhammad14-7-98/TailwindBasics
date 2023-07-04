d42b8a9a133d1c023addb47505efec64a7f9ce51
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2023-07-04 18:42:45 +0500

    Colors and adding custom colors

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      {" "}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-green-600">Hello world!</h1>
+
+      <h1 className="text-yellow-400">Hello world!</h1>
+
+      <h1 className="bg-green-500 text-white">Hello world!</h1>
+
+      <h1 className="border-2 border-green-700">Hello world!</h1>
+
+      <h1 className="text-vuejs">Hello world!</h1>
     </>
   );
 }
